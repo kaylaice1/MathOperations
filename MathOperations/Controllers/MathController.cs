19fdd078a8cbf518233bd9287ea3873184996bb5
KayLynn Johnson <johnson.kaylynn@outlook.com>
2024-09-23 11:08:31 -0500

    Add project files.

---
+++ MathOperations/Controllers/MathController.cs
@@ -0,0 +1,53 @@
+﻿using MathOperations.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MathOperations.Controllers
+{
+    public class MathController : Controller
+    {
+        // Display form to the user
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // Handle form submission and process the operation
+        [HttpPost]
+        public IActionResult Calculate(MathModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                switch (model.Operation)
+                {
+                    case "Add":
+                        model.Result = model.Number1 + model.Number2;
+                        break;
+                    case "Subtract":
+                        model.Result = model.Number1 - model.Number2;
+                        break;
+                    case "Multiply":
+                        model.Result = model.Number1 * model.Number2;
+                        break;
+                    case "Divide":
+                        if (model.Number2 != 0)
+                        {
+                            model.Result = model.Number1 / model.Number2;
+                        }
+                        else
+                        {
+                            ModelState.AddModelError("", "Division by zero is not allowed.");
+                            return View("Index", model);
+                        }
+                        break;
+                    default:
+                        ModelState.AddModelError("", "Invalid operation.");
+                        return View("Index", model);
+                }
+
+                return View("Result", model);
+            }
+
+            return View("Index", model);
+        }
+    }
+}
---
+++ MathOperations/Models/MathModel.cs
@@ -0,0 +1,11 @@
+﻿namespace MathOperations.Models
+{
+    public class MathModel
+    {
+        // Properties to store user inputs and results
+        public double Number1 { get; set; }
+        public double Number2 { get; set; }
+        public string Operation { get; set; }
+        public double? Result { get; set; }
+    }
+}
